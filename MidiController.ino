0ef6a09923d6342712b3efda33586c4487413742
Steve <44708676+scevangelista@users.noreply.github.com>
2023-07-16 11:48:19 -0300

    Midi

--- MidiController.ino
+++ MidiController.ino
@@ -33,6 +33,19 @@ int nSysL = 0;            // Language: 0-Português, 1-English
 int nSysP = 0;            // Current Preset
 double nSysI = millis();  // Last Interaction
 
+// Midi Variables
+int nCH[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }; // Channel
+int nCT[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }; // Type
+int nCC[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }; // Controller
+int nVL[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }; // Value
+
+int nBT[] = { 40, 41, 42, 43, 44, 45 }; // SW PIN
+int nLD[] = { 30, 31, 32, 33, 34, 35 }; // LED PIN
+int valIni[] = { 0, 0, 0, 0, 0, 0 }; // Initial value of SW
+int valNow[] = { 0, 0, 0, 0, 0, 0 }; // Current value of SW
+int altAte[] = { 0, 0, 0, 0, 0, 0 }; // Reads with new SW value
+int nBtn = 6; // Number of SWs
+int nAte = 10; // Number of reads to change SW value
 
 /**
  * Setup Function
@@ -41,6 +54,16 @@ double nSysI = millis();  // Last Interaction
 void setup(void) {
   // Display Init
   u8g2.begin();
+
+  // MIDI Setup
+  Serial.begin(31250);
+
+  // Foot Switchs
+  for (int i = 0; i < nBtn; i++) {
+    pinMode(nBT[i], INPUT);
+    pinMode(nLD[i], OUTPUT);
+    valNow[i] = valIni[i] = digitalRead(nBT[i]);
+  }
 
   // Encoder Init
   pinMode(ECL, INPUT);
@@ -60,6 +83,9 @@ void setup(void) {
 void loop(void) {
   // Encoder
   encoderRead(false);
+
+  // Execute a Midi
+  midiExec();
 }
 
 
@@ -174,4 +200,59 @@ void ledInit() {
     }
     delay(100);
   }
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+// MIDI functions ///////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Execute a Midi Functions
+ */
+void midiExec(){
+  // Read All SWs  
+  for (int i = 0; i < nBtn; i++) {
+    int pinVal = digitalRead(nBT[i]);
+    if (pinVal != valNow[i]){
+      // Diminui falso positivo
+      if (altAte[i] < nAte){
+        altAte[i]++;
+        continue;
+      }
+      altAte[i] = 0;
+
+      if (valIni[i] != pinVal){        
+        led(nLD[i], 1);
+        midiSend(nCH[i], nCT[i], nCC[i], nVL[i]);
+      }
+      else{
+        led(nLD[i], 0);
+        midiSend(nCH[i + 6], nCT[i + 6], nCC[i + 6], nVL[i + 6]);
+      }
+
+      valNow[i] = pinVal;
+    }
+  }
+}
+
+/**
+ * Send Midi Comand
+ * param int ch Channel
+ * param int ct Command Type 1-PC 2-CC
+ * param int cc Controller
+ * param int vl Value
+ */ 
+void midiSend(int ch, int ct, int cc, int vl) {
+  int data1;
+
+  if(ct == 1){
+    data1 = 0xC0 + ch; //PC
+  }
+  else{
+    data1 = 0xB0 + ch; //CC
+  }
+  Serial.write(data1);
+  Serial.write(cc);
+  Serial.write(vl);
 }
