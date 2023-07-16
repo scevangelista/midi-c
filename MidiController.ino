f9e8d09e9dc9c1af78941faf17385862f2368a32
Steve <44708676+scevangelista@users.noreply.github.com>
2023-07-16 11:17:15 -0300

    Display Config

--- MidiController.ino
+++ MidiController.ino
@@ -3,3 +3,25 @@
 //##                           Midi Controller using Arduino Mega2560                            ##
 //#################################################################################################
 
+#include <U8g2lib.h>
+#include <Wire.h>
+
+// Display Config
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, SCL, SDA, U8X8_PIN_NONE);
+
+
+/**
+ * Setup Function
+ * Configure the System
+ */
+void setup(void) {
+  // Display Init
+  u8g2.begin();
+}
+
+/**
+ * Loop Function
+ */
+void loop(void) {
+
+}
